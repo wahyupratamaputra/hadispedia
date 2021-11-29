b995b01f59fefd42b832b02060886a100208cf37
Wahyu Pratama Putra <wahyumacho666@yahoo.com>
2021-11-29 12:51:57 +0800

    Added timeline on index page

--- js/js.js
+++ js/js.js
@@ -1,4 +1,18 @@
 $( "#saran-button" ).click(function() {
-    $( "#saran" ).slideToggle();
-    console.log('a');
-  });
+  $( "#saran" ).slideToggle();
+});
+
+$(window).scroll(function() {
+  var hT = $('#linimasa').offset().top,
+      hH = $('#linimasa').outerHeight(),
+      wH = $(window).height(),
+      wS = $(this).scrollTop();
+  if (wS > (hT+hH-wH)){
+    $( "#list-tokoh" ).slideUp();
+    $( "#blankspace" ).slideUp();
+  }else{
+    $( "#list-tokoh" ).slideDown();
+    $( "#blankspace" ).slideDown();
+    $(".hadispedia-center").css("min-height", "60vh")
+  }
+});
