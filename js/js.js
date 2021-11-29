3499bde14ddbd3d1d082157933972eb3f6fcddd0
Wahyu Pratama Putra <wahyumacho666@yahoo.com>
2021-11-29 14:16:24 +0800

    Fix scroll for mobile

--- js/js.js
+++ js/js.js
@@ -16,6 +16,24 @@ $(window).bind('mousewheel DOMMouseScroll', function(event){
     $( "#blankspace" ).slideUp();
     $( "#linimasa" ).show();
     $( "#linimasa" ).slideDown();
-    console.log("down");
   }
+});
+
+var ts;
+$(document).bind('touchstart', function (e){
+   ts = e.originalEvent.touches[0].clientY;
+});
+
+$(document).bind('touchend', function (e){
+   var te = e.originalEvent.changedTouches[0].clientY;
+   if(ts > te+5){
+    $( "#list-tokoh" ).slideUp();
+    $( "#blankspace" ).slideUp();
+    $( "#linimasa" ).show();
+    $( "#linimasa" ).slideDown();
+   }else if(ts < te-5){
+    $( "#list-tokoh" ).slideDown();
+    $( "#blankspace" ).slideDown();
+    $( "#linimasa" ).slideUp();
+   }
 });
